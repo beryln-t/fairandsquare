8b479e59d654cc730ea3c38c5d8c72abc49af9de
beryln-t <berylntee@gmail.com>
2023-09-08 01:15:06 +0800

    Project Setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
+
+//define routes
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+//use routes
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <div>Hi world</div>;
+}
+
+export default App;
